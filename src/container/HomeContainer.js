6427efdeccbf30ee878a81d5fc660b1d2d954e49
vinothsmart <s.vinothkanna90@gmail.com>
2021-03-19 15:05:19 +0530

    container added

--- src/container/HomeContainer.js
+++ src/container/HomeContainer.js
@@ -1,7 +1,11 @@
+import { connect } from "react-redux";
+import { addToCart } from "../services/Actions/actions";
 import Home from "../components/Home";
 
-function HomeContainer() {
-  return <Home />;
-}
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = (dispatch) => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps);
 
 export default HomeContainer;
