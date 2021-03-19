2de93fba76de07849ff70f0d263ae89efae479c3
vinothsmart <s.vinothkanna90@gmail.com>
2021-03-19 10:06:40 +0530

    Home component updated

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,6 @@ import HomeContainer from "../container/HomeContainer";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
       <HomeContainer />
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,22 @@
 function Home() {
-  return <h1>Home Component</h1>;
+  return (
+    <>
+      <div className="cart">
+        <h1>Cart : 0</h1>
+      </div>
+
+      <div className="item">
+        <img
+          width="100px"
+          height="80px"
+          src="https://img.global.news.samsung.com/global/wp-content/uploads/2020/09/Galaxy-Fold_main1.jpg"
+        />
+        <span> Samsung </span>
+        <span> Price : $1000.00 </span>
+        <button>Add to cart</button>
+      </div>
+    </>
+  );
 }
 
 export default Home;
